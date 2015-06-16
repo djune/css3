5fcc999bd577f96ccd00ec5637ee63bf86513ec3
djune <djune@qq.com>
2015-06-16 21:30:37 +0800

    N/A

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".rotate").animate({"-webkit-transform":"rotate(50deg)"});
+    // $('.rotate').animate({
+    //     textIndent: 0
+    // }, {
+    //     step: function(now, fx) {
+    //         console.log(now,fx);
+    //         $(this).css('-webkit-transform', 'rotate(' + now + 'deg)');
+    //     },
+    //     duration: 'slow'
+    // },'linear');
+});
